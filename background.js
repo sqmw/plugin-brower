2513bd3afaac6f3a2f63318c2884f03da7b8afdc
sqmw <1951918362@qq.com>
2023-06-11 07:08:05 +0800

    new: finish plugin-block, whick can block ele by keys

---
+++ background.js
@@ -0,0 +1,85 @@
+// this script is executed on the agreed domain page
+console.log("plugin index script");
+const childMinLen = 10
+let isFirstGetFather = true
+// Find the parent node by recursion
+function recurseToGetFather(ele) {
+    if (ele.children.length > childMinLen) {
+        return ele
+    }
+    for (let i = 0; i < ele.children.length; i++) {
+        const result = recurseToGetFather(ele.children.item(i))
+        if(result !== undefined){
+            return result
+        }
+    }
+}
+
+const blockInfoList = ["德华"];
+
+// 这样就可以获取 popup.js 里面存储的localstorage的数据
+chrome.storage.local.get(["blockList"]).then((result) => {
+    blockInfoList.push(...JSON.parse(result["blockList"]))
+});
+
+function checkStrIncludes(strA, strList) {
+    return strList.some(function (str) {
+        return strA.includes(str);
+    });
+}
+
+
+
+// There is no need to loop here, you can add a listener to whether to request a new http or through the browser's API
+let lastChildInfo = {
+    "lenSameCount" : 0,
+    "len":-1
+}
+
+const fatherInfo = {
+    tagName: undefined,
+    className: undefined
+}
+
+function getMainEle() {
+    return document.querySelector("main")??document.querySelector("div[class*='search-page-wrapper']");
+}
+function getNewFather() {
+    if (isFirstGetFather){
+        isFirstGetFather = false;
+        const father = recurseToGetFather(getMainEle())
+        fatherInfo.tagName = father.tagName
+        fatherInfo.className = father.className
+        return father;
+    }else{
+        return document.querySelector(fatherInfo.tagName + "[class='"+ fatherInfo.className + "']")??
+            document.querySelector("div[class='video-list row']")??
+            recurseToGetFather(getMainEle())
+    }
+}
+let fatherMain
+setInterval(()=>{
+    // Determine whether the father was successfully obtained
+    try{
+        fatherMain = getNewFather();
+        if(lastChildInfo.len === fatherMain.children.length){
+            if (lastChildInfo.lenSameCount < 100){
+                lastChildInfo.lenSameCount ++;
+            }else{
+                lastChildInfo.lenSameCount = 0
+                lastChildInfo.len = -1
+                return
+            }
+        }
+        for (let i = 0; i < fatherMain?.children.length; i++) {
+            if(checkStrIncludes(fatherMain.children.item(i).innerHTML, blockInfoList)){
+                console.log("remove")
+                fatherMain.children.item(i).remove()
+            }
+        }
+    }catch (e){
+        console.log(e)
+    }
+    // console.log(fatherMain)
+    // Determine if a new DOM is loaded
+}, 200)
---
+++ popup.js
@@ -0,0 +1,65 @@
+// this script is executing as popup page is opening
+setTimeout(() => {
+    console.log("this is popup script")
+}, 100)
+
+let blockList = []
+
+function getLiEle(val = "", blockIndex) {
+    const lastChild = document.createElement("li");
+    lastChild.setAttribute("class", "list-group-item");
+    lastChild.innerHTML = `
+            <div class="input-group mb-3">
+                  <span class="input-group-text" id="basic-addon1">${blockIndex + 1}</span>
+                  <input seq="${blockIndex}" value="${val}" type="text" class="form-control" placeholder="blockInfo">
+                  <button type="button" class="btn btn-danger">删除</button>
+            </div>
+        `
+    lastChild.querySelector("input").addEventListener("change",(event)=>{
+            blockList.splice(blockIndex,1, event.target.value)
+        }
+    )
+
+    lastChild.querySelector("button").addEventListener("click",(event)=>{
+        event.target.parentElement.style.pointerEvents = "none"
+        event.target.className = "btn btn-secondary"
+        event.target.parentElement.style.backgroundColor = "gray"
+            blockList.splice(blockIndex,1)
+        }
+    )
+    return lastChild
+}
+
+
+window.onload = () => {
+    const infoLiEle = document.getElementById("info");
+    let info_html = ""
+    chrome.storage.local.get(["blockList"]).then((result) => {
+        blockList = JSON.parse(result["blockList"])
+        for (let i = 0; i < blockList.length; i++) {
+            infoLiEle.appendChild(getLiEle(blockList[i], i))
+        }
+    });
+
+    document.getElementById("add").addEventListener("click", () => {
+        infoLiEle.appendChild(getLiEle("", blockList.length))
+        blockList.push("")
+    });
+
+    document.getElementById("save").addEventListener("click", () => {
+        blockList = blockList.filter((item, index, arr)=>{
+           return item.trim() !== ""
+        });
+        chrome.storage.local.set({"blockList": JSON.stringify(blockList)}).then(() => {
+            console.log("local save right");
+        });
+    })
+
+    document.getElementById("clear").addEventListener("click", () => {
+        blockList = []
+        infoLiEle.innerHTML = ""
+        chrome.storage.local.set({"blockList": JSON.stringify(blockList)}).then(() => {
+            console.log("clear right");
+        });
+    })
+}
